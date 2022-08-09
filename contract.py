e0b5b45d1a83e3f4aa0c2f34d55d3856ee390a35
Anders Bergquist <abergquist96@gmail.com>
2022-08-08 21:36:57 -0700

    Add files via upload
    Newest DAO contract

---
+++ contract.py
@@ -0,0 +1,60 @@
+from pyteal import *
+
+
+def approval_program():
+    on_creation = Seq(
+        [
+            App.globalPut(Bytes("Creator"), Txn.sender()),
+            Assert(Txn.application_args.length() == Int(0)),
+            Return(Int(1)),
+        ]
+    )
+
+    is_creator = Txn.sender() == App.globalGet(Bytes("Creator"))
+
+    on_closeout = Return(Int(1))
+        
+    on_register = Return(Int(1))
+
+    nft = Txn.application_args[1]
+
+    choice = Txn.application_args[2]
+
+    senderAssetBalance = AssetHolding.balance(Txn.sender(), Txn.assets[0])
+    
+    on_vote = Seq(
+        [
+            senderAssetBalance,
+            Assert(senderAssetBalance.hasValue()),
+            Assert(senderAssetBalance.value() == Int(1)),
+            App.localPut(Txn.sender(), nft, choice),
+            Return(Int(1)),
+        ]
+    )
+
+    program = Cond(
+        [Txn.application_id() == Int(0), on_creation],
+        [Txn.on_completion() == OnComplete.DeleteApplication, Return(is_creator)],
+        [Txn.on_completion() == OnComplete.UpdateApplication, Return(is_creator)],
+        [Txn.on_completion() == OnComplete.CloseOut, on_closeout],
+        [Txn.on_completion() == OnComplete.OptIn, on_register],
+        [Txn.application_args[0] == Bytes("vote"), on_vote],
+    )
+
+    return program
+
+
+def clear_state_program():
+    program = Return(Int(1))
+
+    return program
+
+
+if __name__ == "__main__":
+    with open("vote_approval.teal", "w") as f:
+        compiled = compileTeal(approval_program(), mode=Mode.Application, version=5)
+        f.write(compiled)
+
+    with open("vote_clear_state.teal", "w") as f:
+        compiled = compileTeal(clear_state_program(), mode=Mode.Application, version=5)
+        f.write(compiled)

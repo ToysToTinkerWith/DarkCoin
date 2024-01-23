c331ad420075b10eebaf54d9258cb1605be96058
Claude Baxter <100107801+claudebaxter@users.noreply.github.com>
2024-01-22 20:07:47 -0600

    Implementing ASAblasters how to guide
    Adding how to play information, link to free version of game that doesn't pay rewards, menu background image, adjusting UI/UX

--- pages/ASAblasters/index.js
+++ pages/ASAblasters/index.js
@@ -23,6 +23,12 @@ export default function ASAblasters(props) {
 
   const [assets, setAssets] = useState([])
 
+  const [isHowToPlayVisible, setIsHowToPlayVisible] = useState(false);
+
+  const toggleHowToPlay = () => {
+    setIsHowToPlayVisible(!isHowToPlayVisible);
+  };
+
   const byteArrayToLong = (byteArray) => {
     var value = 0;
     for ( var i = 0; i < byteArray.length; i++) {
@@ -381,28 +387,151 @@ export default function ASAblasters(props) {
                 transform: 'translate(-50%, -50%)',
                 left: '50%',
                 padding: '16px',
-                maxWidth: '500px',
+                maxWidth: '700px',
+                height: '700px',
                 width: '100%',
                 textAlign: 'center',
-                borderRadius: '15px'
+                borderRadius: '15px', 
+                backgroundImage: "url('/ASAblasters.png')", 
+                backgroundSize: 'cover', 
+                backgroundPosition: 'center'
             }}>
-                <h1 style={{ fontSize: '24px', color: 'red', marginBottom: '0', marginTop: '8px' }}>ASA Blasters </h1>
-                <h1 style={{ fontSize: '16px', color: 'black', marginBottom: '0', marginTop: '8px' }}>Highscore = {highScore} </h1>
+                <h1 style={{ 
+                  fontSize: '24px', 
+                  width: 'fit-content', 
+                  margin: 'auto', 
+                  color: 'red', 
+                  backgroundColor: 'black', 
+                  borderRadius: '15px',
+                  marginBottom: 0, 
+                  marginTop: isHowToPlayVisible ? 0 : '20%',
+                  display: isHowToPlayVisible ? 'none' : 'block',}}>
+                    ASA Blasters 
+                </h1>
+                <h1 style={{ 
+                  fontSize: '16px', 
+                  width: 'fit-content', 
+                  margin: 'auto', 
+                  color: 'red', 
+                  backgroundColor: 'black', 
+                  borderRadius: '15px',
+                  marginBottom: '0', 
+                  marginTop: '8px',
+                  display: isHowToPlayVisible ? 'none' : 'block', }}>
+                    Highscore = {highScore} 
+                </h1>
 
-                <h1 style={{ fontSize: '16px', color: 'black', marginBottom: '0', marginTop: '8px' }}>Contract contains</h1>
+                <button id="howToButton" style={{
+                    marginTop: '12px',
+                    backgroundColor: 'blue',
+                    border: 'none',
+                    borderRadius: '15px',
+                    color: 'white',
+                    padding: '8px 16px',
+                    cursor: 'pointer'
+                }}
+                onClick={toggleHowToPlay}>
+                    {isHowToPlayVisible ? 'Hide How To Play' : 'Show How To Play'}
+                </button>
+
+                <div id="howToPlay" style={{ 
+                  display: isHowToPlayVisible ? 'block' : 'none',
+                  backgroundColor: 'black', 
+                  margin: 'auto', 
+                  width: '625px', 
+                  height: '650px', 
+                  borderRadius: '15px',
+                  color: 'white',
+                  fontSize: '12px',
+                  textAlign: 'left'}}>
+                    <h2>How To Play ASA Blasters:</h2>
+                    <h3>Objective:</h3>
+                    <p>Survive the onslaught! Stay in the center and shoot down incoming enemy ASAs. Aim carefully and prevent them from reaching you!</p>
+                    <h3>Gameplay:</h3>
+                    <p>Enemies spawn at the screen's edge every second and shrink with each shot until they're destroyed. Point and click with the mouse, or tap the screen to fire projectiles. Destroying enemies earns you points and ASAs from the smart contract. Watch out for randomly appearing upgrades that boost your firepower!</p>
+                    <h3>Economy:</h3>
+                    <p>Starting a round costs 1000 Dark Coin, but skilled players can quickly earn rewards to cover this and profit within 5-10 minutes of gameplay. To play the free demo, check out https://asa-blasters.vercel.app/ or click the button below to be redirected.</p>
+                    <h3 style={{ textAlign: 'center' }}>Upgrade Items:</h3>
+                    <div style={{ display: 'flex', flexDirection: 'column'}}>
+                      <img style={{ width: '55px', marginLeft: '20%' }} src="/upgrades/rapidfire.png" alt="Rapid Fire"></img>
+                      <span style={{ marginLeft: '20%' }}>RAPID FIRE: Release a continuous stream of bullets by clicking and holding!</span>
+                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'column'}}>
+                      <img style={{ width: '55px', marginLeft: '20%' }} src="/upgrades/scattershot.png" alt="Scatter Shot"></img>
+                      <span style={{ marginLeft: '20%' }}>SCATTER SHOT: Unleash a wide burst of rounds, making it easier to hit targets!</span>
+                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'column'}}>
+                      <img style={{ width: '55px', marginLeft: '20%' }} src="/upgrades/shield.png" alt="Shield"></img>
+                      <span style={{ marginLeft: '20%' }}>SHIELD GENERATOR: Generate a force field around the player for protection!</span>
+                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'column'}}>
+                      <img style={{ width: '55px', marginLeft: '20%' }} src="/upgrades/bombshot.png" alt="Bombs"></img>
+                      <span style={{ marginLeft: '20%' }}>BOMBSHOT: Shoot and detonate bombs to unleash a shockwave of destruction!</span>
+                    </div>      
+                </div>
+
+                <button id="demoButton" style={{
+                    backgroundColor: 'blue',
+                    border: 'none',
+                    borderRadius: '15px',
+                    color: 'white',
+                    padding: '8px 16px',
+                    cursor: 'pointer',
+                    display: 'block',
+                    margin: 'auto',
+                    marginTop: '12px',
+                }}
+                onClick={() => window.location.href = 'https://asa-blasters.vercel.app/'}>
+                    FREE DEMO
+                </button>
+
+                <h1 style={{ 
+                  fontSize: '16px', 
+                  width: 'fit-content', 
+                  margin: 'auto', 
+                  color: 'red', 
+                  backgroundColor: 'black', 
+                  borderRadius: '15px', 
+                  marginBottom: '0', 
+                  marginTop: '8px' }}>
+                    Contract contains:
+                </h1>
                 <Grid container style={{padding: 10}}>
                 {sortedAssets.map((asset, index) => {
                     console.log(asset)
                     return(
-                        <Grid key={index} item xs={12} sm={6} style={{border: "1px solid black", borderRadius: 15, padding: 10}}>
-                            <h1 style={{ fontSize: '16px', color: 'black'}}> {asset.amount} </h1>
-                            <img src={"/ASAblasters/enemies/" + asset.acceptedImg} style={{width: 50}}/>
-                            <h1 style={{ fontSize: '16px', color: 'black'}}> {asset.unitName} </h1>
+                        <Grid 
+                          key={index} 
+                          item xs={12} 
+                          sm={6} 
+                          style={{border: "1px solid black", 
+                          borderRadius: 15, 
+                          padding: 10}}>
+                            <h1 style={{ 
+                              fontSize: '16px', 
+                              color: 'red',
+                              backgroundColor: 'black',
+                              width: 'fit-content',
+                              margin: 'auto'}}> {asset.amount} 
+                            </h1>
+                            <img 
+                              src={"/enemies/" + asset.acceptedImg} 
+                              style={{width: 50}}/>
+                            <h1 
+                              style={{ 
+                                fontSize: '16px', 
+                                color: 'red',
+                                backgroundColor: 'black',
+                                width: 'fit-content',
+                                margin: 'auto'}}> 
+                                  {asset.unitName} 
+                            </h1>
 
                         </Grid>
                     )
                 })}
                 </Grid>
+                
                 <button id="startButton" style={{
                     marginTop: '12px',
                     backgroundColor: 'blue',
@@ -415,7 +544,7 @@ export default function ASAblasters(props) {
                 onClick={() => {
                     startGame()}}>
                     START 1000
-                    <img src={"/ASAblasters/enemies/DARKCOIN.svg"} style={{width: 25}}/>
+                    <img src={"/enemies/DARKCOIN.svg"} style={{width: 25, display: "flex", margin: "auto"}}/>
 
                 </button>
         </div>
